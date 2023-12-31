18b5097480125dda47cec73e2a08fb64382e6f63
shawon-majid <shawon.majid@gmail.com>
2023-12-31 13:27:14 +0600

    socket.io starter added

--- BakcendTemplate/app.js
+++ BakcendTemplate/app.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const port = 8282
 
-app.use(express.json())
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // enable credentials (cookies, authorization headers)
-}));
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected : ' + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log(socket.id + 'user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log(`${socket.id} : ${msg}`);
+  })
+
+});
 
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is running on port ${port}`);
+server.listen(port, () => {
+  console.log('server running at http://localhost:'+ port);
 });
