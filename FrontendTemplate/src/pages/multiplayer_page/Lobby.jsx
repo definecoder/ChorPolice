f909dcd0bcbe544b9cc0aacbd760a8348899c111
codermehraj <codermehraj@gmail.com>
2024-01-01 10:17:31 +0600

    added lobby

--- FrontendTemplate/src/pages/multiplayer_page/Lobby.jsx
+++ FrontendTemplate/src/pages/multiplayer_page/Lobby.jsx
@@ -2,22 +2,36 @@ import "./Lobby.css";
 import avatars from "../../components/avatars";
 import { Button } from "antd";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Lobby() {
 
     const userData = useLocation().state;
     const navigate = useNavigate();
 
+    const [players , setPlayers] = useState([
+
+        {avatar: 1, username: "mehraj", isHost: true},
+        {avatar: 2, username: "amit", isHost: false},
+        {avatar: 3, username: "sakib", isHost: false},
+        {avatar: 4, username: "akib", isHost: false},
+
+    ]);
+
     return <>
         <center className="lobby-code">LOBBY CODE</center>
         <center className="lobby-code">{userData.roomId}</center>
         <div className="lobby-container">
-        <div className="lobby-card"><img src={avatars[userData.avatar]} alt="" /> <span> {userData.username} </span></div>
-        <div className="lobby-card"><img src={avatars[0]} alt="" /> <span> Mehraj </span> <Button onClick={() => {}}> Kick </Button></div>
-        <div className="lobby-card"><img src={avatars[0]} alt="" /> <span> Mehraj </span> <Button onClick={() => {}}> Kick </Button></div>
-        <div className="lobby-card"><img src={avatars[0]} alt="" /> <span> Mehraj </span> <Button onClick={() => {}}> Kick </Button></div>
+            {players.map((player) => {
+                return <div className="lobby-card"><img src={avatars[player.avatar]} alt="" /> <span> {player.username} </span> {!player.isHost && (<Button onClick={() => {}}> Kick </Button>) }</div>;
+            })}        
         </div>   
-        <center><Button> Start Game </Button></center>
+        <center><Button onClick={
+            navigate("/multiplayer", {state : {
+                ...userData,
+                players: players,
+            }})
+        }> Start Game </Button></center>
     </>;
 
 }
