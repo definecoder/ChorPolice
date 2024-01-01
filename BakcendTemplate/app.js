9c62fc4f1c317e6fcf9e06f8048af42226dcace4
nafi-ullah <shafinnafiullah@gmail.com>
2024-01-01 06:51:17 +0600

    latest

--- BakcendTemplate/app.js
+++ BakcendTemplate/app.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
-const connectedUsers = [];
+const connectedUsers = {};
 
 const io = new Server(server, {
   cors: {
@@ -16,29 +16,30 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+
   
     socket.on("join_room", (data) => {
       socket.join(data);
+    // console.log(data);
      
     });
-    socket.on("user_list", (data) => {
-          console.log(data);
-      });
-  
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
 
-  socket.on("send_guti", (data) => {
-    //console.log(data);
-    socket.to(data.room).emit("receive_guti", data);
-  });
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data);
+      });
+
+     
+
+  
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
 });
+
+
+
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
--- FrontendTemplate/src/pages/friends_page/chat.jsx
+++ FrontendTemplate/src/pages/friends_page/chat.jsx
@@ -6,11 +6,6 @@ function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
 
-  const [currentGuti, setCurrentGuti] = useState("");
-  const [gutiList, setGutiList] = useState([]);
-
-  const [clickedIndex, setClickedIndex] = useState(null);
-
   const sendMessage = async () => {
     if (currentMessage !== "") {
       const messageData = {
@@ -30,55 +25,16 @@ function Chat({ socket, username, room }) {
     }
   };
 
-  const sendGuti = async () => {
-    if (currentGuti !== "") {
-      const gutiData = {
-        room: room,
-        author: username,
-        message: currentGuti,
-
-      };
- 
-      await socket.emit("send_guti", gutiData);
-      setGutiList((list) => [...list, gutiData]);
-      setCurrentGuti("");
-      console.log(gutiList);
-    }
-  };
-
-  const handleButtonClick = (index) => {
-    // Update the state with the clicked index
-    setClickedIndex(index);
-    setCurrentGuti(`Button ${index + 1} clicked`);
-    sendGuti();
-    console.log(index);
-    
-  };
-
-
-
-
   useEffect(() => {
-   
 
+    socket.emit("my_room", { room, username });
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
       
     });
 
     return () => socket.removeListener('receive_message')
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("receive_guti", (data) => {
-      setMessageList((list) => [...list, data]);
-      
-    });
-
-    return () => socket.removeListener('receive_guti')
-  }, [socket]);
-
- 
+  }, [socket, room, username]);
 
   return (
     <Wrapper>
@@ -122,35 +78,15 @@ function Chat({ socket, username, room }) {
         <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
-
     <div>
-      {Array.from({ length: 4 }, (_, index) => (
-        <button id='btn' key={index} onClick={() => handleButtonClick(index)}>
-          Button {index + 1}
-        </button>
-      ))}
-
-      {clickedIndex !== null && (
-        <p>Button {clickedIndex + 1} was clicked!</p>
-      )}
+      <button id="btn" onclick=""></button>
     </div>
-    
-    </Wrapper>
+   </Wrapper>
   );
 }
 
 const Wrapper = styled.section`
-.dummyClick{
-  height: 100px;
-  width: 500px;
-  border: 2px solid green;
-  background-color: red;
-}
-#btn{
-  width: 100px;
-}
 
 `;
-
 
 export default Chat;
